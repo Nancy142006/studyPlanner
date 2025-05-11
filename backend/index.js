78b49b896ee37aedee0034a43b5ee0eaf714bd47
Nancy142006 <Nancy14kapoor2006@gmail.com>
2025-05-11 20:16:46 +0530

    modified index file

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+require('./config/db');
+
+const express= require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() => {
+  console.log(`Server running on port ${PORT}`);
+});
